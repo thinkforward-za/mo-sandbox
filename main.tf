98089ab59b7ebd7966da3d5e56c1a1f7b6d46792
= <=>
2026-03-11 11:55:24 +0200

    Add terraform files for S3 Bucket creation

---
+++ main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "mo-sandbox-s3" {
+  bucket = "mo-sandbox-s3-bucket-ga"
+}
---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "aws_access_key" {
+    description = "AWS Access Key"
+  type = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type = string
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type = string
+}
